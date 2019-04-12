5294e40dc11b513b254728dfd3e24c262677cc0a
chenghongqiang <962691576@qq.com>
2019-04-12 17:35:36 +0800

    提交

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,4 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_API: '"http://112.253.11.114:9000/"',
+  // BASE_API: '"http://localhost:9000/"',
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,4 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   BASE_API: '"http://112.253.11.114:9000/"',
+  // BASE_API: '"http://localhost:9000/"',
 }
